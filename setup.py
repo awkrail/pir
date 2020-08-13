4fb745f82f7212684072ba0cf0a8d631f6530c00
misogil0116 <lack_un@yahoo.co.jp>
2020-08-13 22:22:02 +0900

    v0.0.1

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup
 
 setup(
+    name="pir",
+    version="0.0.1",
+    description="A small package",
+    author="misogil0116",
     install_requires=["fabric3", "numpy", "argparse", "pillow"],
     entry_points={
         "console_scripts": [
